7b145c0830974b00940d9180ef9c5f317dd49b5d
kyryloliinyk <kyryloliinyk@gmail.com>
2024-07-31 17:08:52 +0300

    Updateseeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,19 @@ Field.destroy_all
 factory = RGeo::Geographic.spherical_factory(srid: 4326)
 
 polygon1 = factory.polygon(factory.linear_ring([
-                                                 factory.point(-90.0, 30.0),
-                                                 factory.point(-90.0, 35.0),
-                                                 factory.point(-85.0, 35.0),
-                                                 factory.point(-85.0, 30.0),
-                                                 factory.point(-90.0, 30.0)
+                                                 factory.point(24.0, 48.0),
+                                                 factory.point(24.0, 49.0),
+                                                 factory.point(25.0, 50.0),
+                                                 factory.point(26.0, 48.0),
+                                                 factory.point(24.0, 48.0)
                                                ]))
 
 polygon2 = factory.polygon(factory.linear_ring([
-                                                 factory.point(-80.0, 25.0),
-                                                 factory.point(-80.0, 30.0),
-                                                 factory.point(-75.0, 30.0),
-                                                 factory.point(-75.0, 25.0),
-                                                 factory.point(-80.0, 25.0)
+                                                 factory.point(26.5, 48.0),
+                                                 factory.point(27.0, 49.0),
+                                                 factory.point(27.5, 50.0),
+                                                 factory.point(26.0, 50.0),
+                                                 factory.point(26.5, 48.0)
                                                ]))
 
 shape = factory.multi_polygon([polygon1, polygon2])
