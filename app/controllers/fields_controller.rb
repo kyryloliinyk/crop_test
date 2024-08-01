7e8b3661e69655a490b8087fd945e769d4037378
kyryloliinyk <kyryloliinyk@gmail.com>
2024-08-01 10:43:17 +0300

    add ability to create new field with name

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -14,7 +14,11 @@ class FieldsController < ApplicationController
     @geojson = @field.geojson_shape.to_json
   end
 
-  def create; end
+  def create
+    field = Field.new(field_params)
+    field.save
+    redirect_to fields_path
+  end
 
   def edit
     @field = Field.find(params.require(:id))
@@ -25,5 +29,11 @@ class FieldsController < ApplicationController
   def destroy
     Field.find(params.require(:id)).delete
     redirect_to fields_path
+  end
+
+  private
+
+  def field_params
+    params.require(:field).permit(:name, :shape, :area)
   end
 end
